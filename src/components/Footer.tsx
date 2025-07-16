31fe46dc9b497fbd1083fac6fb0179c5ab2e0519
OmarLawaty <omarlawatey@gmail.com>
2025-07-16 13:52:13 +0300

    feat: Implement footer responsiveness for all screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,19 @@ import { Box, Flex, type FlexProps } from '@chakra-ui/react';
 import { ContactLinks } from './ContactLinks';
 
 export const Footer = (props: FlexProps) => (
-  <Flex align='center' justify='space-between' w='full' bg='gray.800' py='5' px='12' rounded='10rem' my='4' {...props}>
-    <Box color='gray.400' fontSize='1.25rem'>
+  <Flex
+    align='center'
+    justify='space-between'
+    w='full'
+    bg='gray.800'
+    py={[3, 4, 5]}
+    px={[6, 8, 12]}
+    rounded='10rem'
+    my='4'
+    mb={[16, 4]}
+    {...props}
+  >
+    <Box color='gray.400' fontSize={['1rem', '1.125rem', '1.25rem']} fontFamily="'Nunito', sans-serif">
       Contact me
     </Box>
 
