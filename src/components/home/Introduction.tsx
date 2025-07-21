b51464b16bea9253226f61c63a256b8914908986
OmarLawaty <omarlawatey@gmail.com>
2025-07-21 11:14:26 +0300

    feat: Implement Home page introduction section responsiveness for all screens

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -7,23 +7,44 @@ import { Image } from '../Image';
 import { Link } from '../Link';
 
 export const Introduction = () => (
-  <Flex as='section' w='full' gap='24' align='center' justify='space-between' h='fit'>
-    <Flex flexDir='column' gap='1'>
-      <Heading as='h1' color='white' fontSize='4.25rem' lineHeight='normal' fontWeight='600' fontFamily='inherit'>
+  <Flex
+    flexDir={['column-reverse', null, 'row']}
+    as='section'
+    w='full'
+    gap={[12, 16, 24]}
+    align='center'
+    justify='space-between'
+    h='fit'
+  >
+    <Flex flexDir='column' gap='1' textAlign={['center', null, 'left']} align={['center', null, 'flex-start']}>
+      <Heading
+        as='h1'
+        color='white'
+        fontSize={['2rem', '3.125rem', '4.25rem']}
+        lineHeight='normal'
+        fontWeight='600'
+        fontFamily='inherit'
+      >
         Hi, I&apos;m Omar
       </Heading>
 
-      <Heading as='h2' color='purple.500' fontWeight='500' fontSize='1.25rem' fontFamily='inherit'>
+      <Heading
+        as='h2'
+        color='purple.500'
+        fontWeight='500'
+        fontSize={['1rem', '1.125rem', '1.25rem']}
+        fontFamily='inherit'
+      >
         Frontend Developer & CS student
       </Heading>
 
-      <Text mt='2' color='gray.3d00' fontWeight='300' fontSize='1rem' maxW='38rem'>
+      <Text mt='2' color='gray.3d00' fontWeight='300' fontSize={['0.875rem', '0.95rem', '1rem']} maxW='38rem'>
         I&apos;ve been building real-world web apps for about a year using React, Next.js, and TypeScript — with a
         strong focus on performance and clean UI. I&apos;m currently open to remote internships or part-time roles where
         I can grow and contribute to meaningful projects.
       </Text>
 
-      <Flex mt='5' gap='5'>
+      <Flex mt={[8, null, 5]} gap={[3, 4, 5]} flexDir={['column', null, 'row']} w={['fit', null, 'auto']}>
         <DownloadResumeButton />
 
         <Link
@@ -51,8 +72,8 @@ export const Introduction = () => (
       priority
       fetchPriority='high'
       quality={50}
-      w='21rem'
-      h='21rem'
+      w={['18rem', '21rem']}
+      h={['18rem', '21rem']}
       aspectRatio='square'
       objectFit='cover'
       objectPosition='0 20%'
