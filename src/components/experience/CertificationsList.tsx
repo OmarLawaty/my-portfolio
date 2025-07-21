cf5e64e2eff2d8e4145452c7974516d45f648e60
OmarLawaty <omarlawatey@gmail.com>
2025-07-21 10:42:00 +0300

    feat: Implement experience page certifications section responsiveness for all screens

--- src/components/experience/CertificationsList.tsx
+++ src/components/experience/CertificationsList.tsx
@@ -13,12 +13,20 @@ export const CertificationsList = () => {
   if (!experienceQuery.isSuccess) return null;
 
   return (
-    <Flex as='section' flexDir='column' gap='16' w='full'>
-      <Heading as='h2' color='white' fontWeight='600' fontSize='2.25rem' lineHeight='normal' px='12'>
+    <Flex as='section' flexDir='column' gap={[12, 14, 16]} w='full'>
+      <Heading
+        as='h2'
+        color='white'
+        fontWeight='600'
+        fontSize={['1.5rem', '1.75rem', '2.25rem']}
+        lineHeight='normal'
+        px={['none', null, 12]}
+        textAlign={['center', null, 'start']}
+      >
         Certifications
       </Heading>
 
-      <Grid templateColumns='repeat(auto-fit, minmax(min(400px, 100%), 1fr))' gap='12' w='full'>
+      <Grid templateColumns='repeat(auto-fit, minmax(min(400px, 100%), 1fr))' gap={[8, 10, 12]} w='full'>
         {experienceQuery.data.certifications.map(certification => (
           <CertificationCard key={certification.title} certification={certification} />
         ))}
@@ -32,16 +40,31 @@ interface CertificationCardProps extends FlexProps {
 }
 
 const CertificationCard = ({ certification, ...props }: CertificationCardProps) => (
-  <Flex flexDir='column' gap='1.5' px='12' py='8' bg='gray.800' rounded='1.5rem' textTransform='capitalize' {...props}>
-    <Heading as='h3' fontWeight='600' fontSize='1.4rem'>
+  <Flex
+    flexDir='column'
+    gap='1.5'
+    px={[6, 8, 12]}
+    py={[6, null, 8]}
+    bg='gray.800'
+    rounded='1.5rem'
+    textTransform='capitalize'
+    {...props}
+  >
+    <Heading as='h3' fontWeight='600' fontSize={['1rem', '1.125rem', '1.4rem']}>
       {certification.title}
     </Heading>
 
-    <Text color='purple.300' fontSize='1rem'>
+    <Text color='purple.300' fontSize={['0.8rem', '0.9rem', '1rem']}>
       {certification.authority}
     </Text>
 
-    <Text mt='0.5' fontWeight='200' fontSize='0.9rem' color='gray.400' fontFamily="'Nunito', sans-serif">
+    <Text
+      mt='0.5'
+      fontWeight='200'
+      fontSize={['0.75rem', '0.8rem', '0.9rem']}
+      color='gray.400'
+      fontFamily="'Nunito', sans-serif"
+    >
       {certification.date}
     </Text>
 
@@ -52,7 +75,7 @@ const CertificationCard = ({ certification, ...props }: CertificationCardProps) 
       w='fit'
       color='green.500'
       fontWeight='500'
-      fontSize='1rem'
+      fontSize={['0.8rem', '0.9rem', '1rem']}
       _hover={{ textDecoration: 'underline' }}
     >
       View Certificate
