fd11517a76fa5d00910f0087f50901377ed61a2a
OmarLawaty <omarlawatey@gmail.com>
2025-07-21 10:41:17 +0300

    feat: Implement experience page work experience section responsiveness for all screens

--- src/components/experience/WorkList.tsx
+++ src/components/experience/WorkList.tsx
@@ -11,12 +11,20 @@ export const WorkList = () => {
   if (!experienceQuery.isSuccess) return null;
 
   return (
-    <Flex as='section' flexDir='column' gap='16' w='full'>
-      <Heading as='h2' color='white' fontWeight='600' fontSize='2.25rem' lineHeight='normal' px='12'>
+    <Flex as='section' flexDir='column' gap={[12, 14, 16]} w='full'>
+      <Heading
+        as='h2'
+        color='white'
+        fontWeight='600'
+        fontSize={['1.5rem', '1.75rem', '2.25rem']}
+        lineHeight='normal'
+        px={['none', null, 12]}
+        textAlign={['center', null, 'start']}
+      >
         Work Experience
       </Heading>
 
-      <Flex flexDir='column' gap='12' w='full'>
+      <Flex flexDir='column' gap={[8, 10, 12]} w='full'>
         {experienceQuery.data.work.map(work => (
           <WorkCard key={work.title} work={work} />
         ))}
@@ -30,17 +38,27 @@ interface WorkCardProps extends FlexProps {
 }
 
 const WorkCard = ({ work, ...props }: WorkCardProps) => (
-  <Flex gap='8' px='14' py='10' bg='gray.800' rounded='3rem' flex='1' textTransform='capitalize' {...props}>
-    <Flex flexDir='column' flex='1' minW='20rem' gap='4'>
-      <Heading as='h3' color='white' fontWeight='600' fontSize='1.5rem' lineHeight='normal'>
+  <Flex
+    flexDir={['column', null, 'row']}
+    gap={[4, 6, 8]}
+    px={[8, 12, 14]}
+    py={[8, null, 10]}
+    bg='gray.800'
+    rounded={['2rem', null, '3rem']}
+    flex='1'
+    textTransform='capitalize'
+    {...props}
+  >
+    <Flex flexDir='column' flex='1' minW='20rem' gap={[1, 2, 4]}>
+      <Heading as='h3' color='white' fontWeight='600' fontSize={['1rem', '1.125rem', '1.5rem']} lineHeight='normal'>
         {work.role}
       </Heading>
 
-      <Text color='purple.300' fontSize='1.125rem' fontWeight='400'>
+      <Text color='purple.300' fontSize={['0.9rem', '1rem', '1.125rem']} fontWeight='400'>
         {work.type}
       </Text>
 
-      <Flex flexDir='column' gap='1' fontWeight='200' fontSize='0.9rem' color='gray.400'>
+      <Flex flexDir='column' gap='1' fontWeight='200' fontSize={['0.75rem', '0.8rem', '0.9rem']} color='gray.400'>
         <Text fontFamily="'Nunito', sans-serif">
           {work.startDate}{' '}
           <Text as='span' color='green.500'>
@@ -53,8 +71,8 @@ const WorkCard = ({ work, ...props }: WorkCardProps) => (
       </Flex>
     </Flex>
 
-    <Flex flexDir='column' flex='3' gap='3' fontSize='1rem'>
-      <Text color='purple.300' fontWeight='600' fontSize='1.125rem'>
+    <Flex flexDir='column' flex='3' gap={[1, null, 3]} fontSize={['0.9rem', null, '1rem']}>
+      <Text color='purple.300' fontWeight='600' fontSize={['0.9rem', '1rem', '1.125rem']}>
         {work.title}
       </Text>
 
