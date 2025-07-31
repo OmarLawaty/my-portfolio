3f4f437bb4fda8f97456dac9bc519508b4d02733
OmarLawaty <omarlawatey@gmail.com>
2025-07-31 20:40:35 +0300

    feat: Add hover effect to project cards in projects pages

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -14,7 +14,15 @@ export const ProjectsList = () => {
   return (
     <Flex as='section' wrap='wrap' gap='10'>
       {reposQuery.data.map(repo => (
-        <ProjectCard key={repo.name} repo={repo} flex='1 1 30rem' minW='auto' maxW='unset' />
+        <ProjectCard
+          key={repo.name}
+          repo={repo}
+          flex='1 1 30rem'
+          whileHover={{ scale: 1.05, boxShadow: '0 0 2rem rgba(255, 255, 255, 0.2)' }}
+          transition={{ scale: { duration: 0.5, ease: 'easeInOut' }, boxShadow: { duration: 0.2, ease: 'easeInOut' } }}
+          minW='auto'
+          maxW='unset'
+        />
       ))}
     </Flex>
   );
